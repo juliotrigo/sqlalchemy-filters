6eb1f9053b2dd8323757c0684d1a6d95416f9031
Julio Trigo <julio.trigo@student.com>
2016-07-19 09:10:11 +0100

    PLAT-116-PLAT-1411 - Obtain operator arity from operator function

--- sqlalchemy_filters/filters.py
+++ sqlalchemy_filters/filters.py
@@ -1,42 +1,39 @@
 # -*- coding: utf-8 -*-
 
-from collections import namedtuple
+from inspect import signature
 
 from .exceptions import BadFilterFormat, BadQuery
 
 
 class Operator(object):
 
-    OperatorFunction = namedtuple('OperatorFunction', ['arity', 'function'])
-
     OPERATORS = {
-        'is_null': OperatorFunction(1, lambda f: f.is_(None)),
-        'is_not_null': OperatorFunction(1, lambda f: f.isnot(None)),
-        '==': OperatorFunction(2, lambda f, a: f == a),
-        'eq': OperatorFunction(2, lambda f, a: f == a),
-        '!=': OperatorFunction(2, lambda f, a: f != a),
-        'ne': OperatorFunction(2, lambda f, a: f != a),
-        '>': OperatorFunction(2, lambda f, a: f > a),
-        'gt': OperatorFunction(2, lambda f, a: f > a),
-        '<': OperatorFunction(2, lambda f, a: f < a),
-        'lt': OperatorFunction(2, lambda f, a: f < a),
-        '>=': OperatorFunction(2, lambda f, a: f >= a),
-        'ge': OperatorFunction(2, lambda f, a: f >= a),
-        '<=': OperatorFunction(2, lambda f, a: f <= a),
-        'le': OperatorFunction(2, lambda f, a: f <= a),
-        'like': OperatorFunction(2, lambda f, a: f.like(a)),
-        'in': OperatorFunction(2, lambda f, a: f.in_(a)),
-        'not_in': OperatorFunction(2, lambda f, a: ~f.in_(a)),
+        'is_null': lambda f: f.is_(None),
+        'is_not_null': lambda f: f.isnot(None),
+        '==': lambda f, a: f == a,
+        'eq': lambda f, a: f == a,
+        '!=': lambda f, a: f != a,
+        'ne': lambda f, a: f != a,
+        '>': lambda f, a: f > a,
+        'gt': lambda f, a: f > a,
+        '<': lambda f, a: f < a,
+        'lt': lambda f, a: f < a,
+        '>=': lambda f, a: f >= a,
+        'ge': lambda f, a: f >= a,
+        '<=': lambda f, a: f <= a,
+        'le': lambda f, a: f <= a,
+        'like': lambda f, a: f.like(a),
+        'in': lambda f, a: f.in_(a),
+        'not_in': lambda f, a: ~f.in_(a),
     }
 
     def __init__(self, operator):
         if operator not in self.OPERATORS:
             raise BadFilterFormat('Operator `{}` not valid.'.format(operator))
 
-        op = self.OPERATORS[operator]
         self.operator = operator
-        self.arity = op.arity
-        self.function = op.function
+        self.function = self.OPERATORS[operator]
+        self.arity = len(signature(self.function).parameters)
 
 
 class Field(object):
