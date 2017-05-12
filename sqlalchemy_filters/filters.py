ea7091f8a88637647214669d10fa2b90c4b57dbe
timbu <tim.buckland@student.com>
2017-05-12 14:43:36 +0100

    rename to build_sqlalchemy_filters

--- sqlalchemy_filters/filters.py
+++ sqlalchemy_filters/filters.py
@@ -85,15 +85,14 @@ class Filter(object):
             return function(field, self.value)
 
 
-def assemble_filters(filterdef, models):
+def build_sqlalchemy_filters(filterdef, models):
     """ Recursively parse the `filterdef` into sqlalchemy filter arguments """
 
     if isinstance(filterdef, (list, tuple)):
-        return [assemble_filters(item, models) for item in filterdef]
+        return [build_sqlalchemy_filters(item, models) for item in filterdef]
 
     if isinstance(filterdef, dict):
         # Check if filterdef defines a boolean function.
-
         for key, boolean_fn, only_one_arg in BOOLEAN_FUNCTIONS:
             if key in filterdef:
                 # The filterdef is for a boolean-function
@@ -114,7 +113,7 @@ def assemble_filters(filterdef, models):
                         'length > 1'.format(key)
                     )
 
-                return boolean_fn(*assemble_filters(fn_args, models))
+                return boolean_fn(*build_sqlalchemy_filters(fn_args, models))
 
     return Filter(filterdef, models).format_for_sqlalchemy()
 
@@ -141,7 +140,7 @@ def apply_filters(query, filters):
     if not isinstance(filters, (list, tuple)):
         raise BadFilterFormat('`filters` must be a list or tuple')
 
-    sqlalchemy_filters = assemble_filters(filters, models)
+    sqlalchemy_filters = build_sqlalchemy_filters(filters, models)
 
     if sqlalchemy_filters:
         query = query.filter(*sqlalchemy_filters)
