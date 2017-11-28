bd91a4be7a57b5fe630b556cbe7bd3618a53764a
Matt Bennett <matt@bennett.name>
2017-11-28 08:13:34 +0000

    explicitly test get_model_from_spec

--- test/interface/test_models.py
+++ test/interface/test_models.py
@@ -1,7 +1,9 @@
+import pytest
 from sqlalchemy import func
 from sqlalchemy.orm import joinedload
 
-from sqlalchemy_filters import get_query_models
+from sqlalchemy_filters.exceptions import BadSpec, BadQuery
+from sqlalchemy_filters.models import get_query_models, get_model_from_spec
 from test.models import Bar, Foo, Qux
 
 
@@ -77,3 +79,40 @@ class TestGetQueryModels(object):
 
         # Bar is not added to the query since the joinedload is transparent
         assert {'Foo': Foo} == entities
+
+
+class TestGetModelFromSpec:
+
+    def test_query_with_no_models(self, session):
+        query = session.query()
+        spec = {'field': 'name', 'op': '==', 'value': 'name_1'}
+
+        with pytest.raises(BadQuery) as err:
+            get_model_from_spec(spec, query)
+
+        assert 'The query does not contain any models.' == err.value.args[0]
+
+    def test_query_with_named_model(self, session):
+        query = session.query(Bar)
+        spec = {'model': 'Bar'}
+
+        model = get_model_from_spec(spec, query)
+        assert model == Bar
+
+    def test_query_with_missing_named_model(self, session):
+        query = session.query(Bar)
+        spec = {'model': 'Buz'}
+
+        with pytest.raises(BadSpec) as err:
+            get_model_from_spec(spec, query)
+
+        assert 'The query does not contain model `Buz`.' == err.value.args[0]
+
+    def test_multiple_models_ambiquous_spec(self, session):
+        query = session.query(Bar, Qux)
+        spec = {'field': 'name', 'op': '==', 'value': 'name_1'}
+
+        with pytest.raises(BadSpec) as err:
+            get_model_from_spec(spec, query)
+
+        assert 'Ambiguous spec. Please specify a model.' == err.value.args[0]
