236f676527285fe84460db58db1d2aab7467ee55
Matt Bennett <matt@bennett.name>
2017-11-28 08:24:57 +0000

    remove tests that re-cover get_model_from_spec

--- sqlalchemy_filters/__init__.py
+++ sqlalchemy_filters/__init__.py
@@ -2,6 +2,5 @@
 
 from .filters import apply_filters  # noqa: F401
 from .loads import apply_loads  # noqa: F401
-from .models import get_query_models  # noqa: F401
 from .pagination import apply_pagination  # noqa: F401
 from .sorting import apply_sort  # noqa: F401
