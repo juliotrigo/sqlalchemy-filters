9fc7ef7708e93238a73b1b16ba3cada3ea6f9070
Julio Trigo <julio.trigo@gmail.com>
2019-03-06 20:35:18 +0000

    Bump up version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as handle:
 
 setup(
     name='sqlalchemy-filters',
-    version='0.8.0',
+    version='0.9.0',
     description='A library to filter SQLAlchemy queries.',
     long_description=readme,
     author='Student.com',
