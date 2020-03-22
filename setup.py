b87d29110a1d04c873b892eac3930309b2881fb1
Julio Trigo <julio.trigo@gmail.com>
2020-03-22 16:51:03 +0000

    Support Python 3.8

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Database",
         "Topic :: Database :: Front-Ends",
         "Topic :: Software Development :: Libraries :: Python Modules",
