364f229fc17fd49f3f173e279eb5aa93f05d675c
Tom V <tom.viner@sohonet.com>
2023-04-13 11:10:56 +0100

    set python_requires='>=3.7'

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     author_email='wearehiring@student.com',
     url='https://github.com/juliotrigo/sqlalchemy-filters',
     packages=find_packages(exclude=['test', 'test.*']),
+    python_requires='>=3.7',
     install_requires=['sqlalchemy>=1.0.16', 'six>=1.10.0'],
     extras_require={
         'dev': [
